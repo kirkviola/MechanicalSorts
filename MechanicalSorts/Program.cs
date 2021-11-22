27e0d0ac9cfb2bf3e58ed4b63fabb4ace660d9b1
Matthew Kirkendall <kirk.viola@gmail.com>
2021-11-22 16:08:17 -0500

    Updated to let user make a list of numbers

--- MechanicalSorts/Program.cs
+++ MechanicalSorts/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var NumList = new List<int>() { 1, -24, 13, 100, -45, 12};
+var NumList = ListBuilder();
 
 var FinishedList = BubbleSort(NumList);
 foreach(var Item in FinishedList)
@@ -8,7 +8,6 @@ foreach(var Item in FinishedList)
 List<int> BubbleSort(List<int> list)
 {
     
-
     for(var i = list.Count; i >= 0; i--)
     {
         for(var z = 0; z < i - 1; z++)
@@ -26,4 +25,33 @@ List<int> BubbleSort(List<int> list)
         }
     }
     return list;
+}
+
+List<int> ListBuilder()
+{
+    var done = false;
+    var nums = new List<int>();
+    while (!done)
+    {
+        Console.WriteLine("Type a number you wish to add to you list, press n to complete:");
+        var num = Console.ReadLine();
+        if (num.ToLower() == "n")
+        {
+            done = true;
+            continue;
+        }
+        else
+        {
+            try
+            {
+                var testNum = Convert.ToInt32(num);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Invalid character");
+                continue;
+            }
+            nums.Add(Convert.ToInt32(num));
+        }
+    }
+    return nums;
 }
